c764f7368cce20e67118d1f7d9c831fb4d3bc988
sahil shaikh <jazim2211@gmail.com>
2022-08-15 18:40:03 +0530

    added filder

---
+++ DDL/sp/sp_change_name.sql
@@ -0,0 +1,4 @@
+select * from __BQ_PROJECT__.data_set_name.table_name;
+select * from __BQ_PROJECT__.data_set_name.table_name;
+select * from __BQ_PROJECT__.data_set_name.table_name;
+select * from __BQ_PROJECT__.data_set_name.table_name;
